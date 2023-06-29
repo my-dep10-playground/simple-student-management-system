f24783b7a2323885eb0f7f6a2c6ab87808f1497c
Gayantha Kuruppuarachchi <gayantha355@gmail.com>
2023-06-29 19:24:14 +0530

    set up folder structure with spring boot

---
+++ simple-sms/src/main/java/lk/ijse/dep10/app/SimpleSmsApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSmsApplication.class, args);
+	}
+
+}
